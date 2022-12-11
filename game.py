074bda0534754a9084fade3b48eaa28941f84246
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-11 09:53:55 -0500

    deal_hand method

--- game.py
+++ game.py
@@ -1,11 +1,21 @@
+import random
+
+
 class BlackJack :
+
+    _deck = [2,3,4,5,6,7,8,9,10, "J", "K", "A", "Q"]
+    _hand_of_player = []
+    _hand_of_dealer = []
 
     def __init__(self):
         print("init game")
 
 
-    
-
+    def deal_hand(self, turn) :
+        card = random.choice( len( self._deck ) - 1 )
+        turn.append(card)
+        self._deck.remove(card)
+        
 
 
 
